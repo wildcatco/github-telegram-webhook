d08a3a9778b9dbeff0c813c79b8a269bcde13b2a
Jihoo Kim <jihoo94@gmail.com>
2023-10-19 18:48:54 +0900

    요청 유효성 검사

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 const express = require("express");
+const { verifySignature } = require("./utils");
+require("dotenv").config();
 
 const app = express();
+const secret = process.env.SECRET;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello!");
-});
+app.post("/webhook", (req, res) => {
+  const isValidRequest = verifySignature(
+    secret,
+    req.header("X-Hub-Signature-256"),
+    req.body
+  );
+  if (!isValidRequest) {
+    return res.status(400).send("Not valid request");
+  }
 
-app.post("/", (req, res) => {
-  res.json(req.body);
+  // const githubEvent = req.headers["x-github-event"];
+  //
+  // if (githubEvent === "issues") {
+  //   const data = req.body;
+  //   const action = data.action;
+  //   if (action === "opened") {
+  //     console.log(`An issue was opened with this title: ${data.issue.title}`);
+  //   } else if (action === "closed") {
+  //     console.log(`An issue was closed by ${data.issue.user.login}`);
+  //   } else {
+  //     console.log(`Unhandled action for the issue event: ${action}`);
+  //   }
+  // } else if (githubEvent === "ping") {
+  //   console.log("GitHub sent the ping event");
+  // } else {
+  //   console.log(`Unhandled event: ${githubEvent}`);
+  // }
+
+  res.status(202).send("Accepted!");
 });
 
 app.listen(3000, () => {
